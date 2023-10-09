0a7f79cdc62af80f43454fc333e13d31ba127246
shivraj0002 <95290113+shivraj0002@users.noreply.github.com>
2023-10-09 10:01:13 +0530

    initilized project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import Dropdown from "./Dropdown/dropdown";
+
+function App() {
+  return (
+    <div className="app">
+      Welcome to the react app
+    </div>
+  );
+}
+
+export default App;
